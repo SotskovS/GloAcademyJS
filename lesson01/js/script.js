f010918ce8bdcd6210917fbfd7b50c2e46b657ac
s <svalgele@gmail.com>
2020-08-07 22:09:10 +0300

    add var:values, str => lowerCase, array

--- lesson01/js/script.js
+++ lesson01/js/script.js
@@ -1,15 +1,17 @@
-let money;
+let money = 50_000;
+let income = 'Freelance'; 
+let addExpenses = 'Интернет, Такси, Коммуналка';
+let deposit = true;
+let mission = 800_000;
+let period = 12;
+let budgetDay = money / 30;
 
-let income; 
+console.log('money: ', typeof money);
+console.log('income: ', typeof income);
+console.log('deposit: ', typeof deposit);
+console.log('addExpenses length: ', addExpenses.length);
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать ${mission} рублей`);
 
-let addExpenses;
-
-let deposit;
-
-let mission;
-
-let period;
-
-alert('First message');
-
-console.log('First message');
+let expensesArray = addExpenses.toLowerCase().split(', ');
+console.log(expensesArray);
