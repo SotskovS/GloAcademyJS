14ffcdcb485d6226373255ebef36da4424c2d945
s <svalgele@gmail.com>
2020-08-08 23:23:34 +0300

    refactor: const, print type var

--- lesson01/js/script.js
+++ lesson01/js/script.js
@@ -1,17 +1,16 @@
-let money = 50_000;
-let income = 'Freelance'; 
-let addExpenses = 'Интернет, Такси, Коммуналка';
-let deposit = true;
-let mission = 800_000;
-let period = 12;
-let budgetDay = money / 30;
+const money = 50_000,
+  income = 'Freelance',
+  addExpenses = 'Интернет, Такси, Коммуналка',
+  deposit = true,
+  mission = 800_000,
+  period = 12,
+  budgetDay = money / 30;
 
-console.log('money: ', typeof money);
-console.log('income: ', typeof income);
-console.log('deposit: ', typeof deposit);
+console.log('money:', typeof money, ' income:', typeof income, ' deposit:', typeof deposit);
+
 console.log('addExpenses length: ', addExpenses.length);
 console.log(`Период равен ${period} месяцев`);
 console.log(`Цель заработать ${mission} рублей`);
 
-let expensesArray = addExpenses.toLowerCase().split(', ');
-console.log(expensesArray);
+console.log(addExpenses.toLowerCase().split(', '));
+console.log(budgetDay);
